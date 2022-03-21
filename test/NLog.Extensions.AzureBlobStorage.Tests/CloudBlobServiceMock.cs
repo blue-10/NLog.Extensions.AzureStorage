582bbfa37931009f6c0512b9346b3d679f2efb7c
Archomeda <ds@blue10.com>
2022-03-21 11:41:43 +0100

    Fix interface

--- test/NLog.Extensions.AzureBlobStorage.Tests/CloudBlobServiceMock.cs
+++ test/NLog.Extensions.AzureBlobStorage.Tests/CloudBlobServiceMock.cs
@@ -17,7 +17,7 @@ namespace NLog.Extensions.AzureBlobStorage.Tests
 
         public IDictionary<string, string> BlobTags { get; private set; }
 
-        public void Connect(string connectionString, string serviceUri, string tenantIdentity, string resourceIdentity, IDictionary<string, string> blobMetadata, IDictionary<string, string> blobTags)
+        public void Connect(string connectionString, string serviceUri, string tenantIdentity, IDictionary<string, string> blobMetadata, IDictionary<string, string> blobTags)
         {
             ConnectionString = connectionString;
             BlobMetadata = blobMetadata;
